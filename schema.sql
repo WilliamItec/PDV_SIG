44998ae662f5fab7d7d90ae00b90c2e2a13e2852
wildes oliveira <wildes oliveira>
2017-06-21 12:32:31 -0300

    ajustes performance consultas

--- schema.sql
+++ schema.sql
@@ -67,6 +67,66 @@ where
 
 $$
 
+drop view if exists v_cupomfiscal_crediario;
+
+$$
+
+create view v_cupomfiscal_crediario
+as
+select 
+cd_emp,cd_filial,cd_cx,dt_vd,cd_vd
+from
+pdv_vd
+where 
+1 = 2
+
+$$
+
+drop view if exists v_cupomfiscal_cheque;
+
+$$
+
+create view v_cupomfiscal_cheque 
+as
+select 
+cd_emp,cd_filial,cd_cx,dt_vd,cd_vd
+from
+pdv_vd
+where 
+1 = 2
+
+$$
+
+drop view if exists v_cupomfiscal_vale_compra;
+
+$$
+
+create view v_cupomfiscal_vale_compra 
+as
+select 
+cd_emp,cd_filial,cd_cx,dt_vd,cd_vd
+from
+pdv_vd
+where 
+1 = 2
+
+$$
+
+drop view if exists v_cupomfiscal_item_receita;
+
+$$
+
+create view v_cupomfiscal_item_receita
+as
+select 
+cd_emp,cd_filial,cd_cx,dt_vd,cd_vd,0 as cd_it
+from
+pdv_vd
+where 
+1 = 2
+
+$$
+
 drop trigger if exists tg_pdv_vd_flag_consolidado_insert;
 
 $$
